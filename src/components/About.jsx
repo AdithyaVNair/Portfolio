4afd51fbb8b3dfebfd25cdd13fe4b9f2d3f1fdf7
AdithyaVNair <adithyavinodnair2004@gmail.com>
2025-12-27 13:35:51 +0530

    made changes in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import aboutPhoto from '../assets/about_photo.jpg';
+import aboutPhoto from '../assets/hero_new.png';
 
 const education = [
     {
